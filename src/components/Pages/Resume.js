bc1134ef136c0427b4561db4d7750e784209e235
Wenwen Tian <wwtian9@gmail.com>
2022-03-31 15:52:37 -0600

    add resume styles

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -1,46 +1,58 @@
 import React from "react";
 import CV from "../../assets/resume/CV_wwt.pdf";
+import download from "../../assets/icons/download.png";
 
 export default function Resume() {
   return (
     <div>
       <h2 className="title">Resume</h2>
-      <a href={CV} download>
-        Download CV
-        {/* --------Frontend Skills------------ */}
-      </a>
-      <div>
-        <h2>Frontend Skills</h2>
-        <div>
-          <span>JavaScript</span>
-          <span>React</span>
-          <span>HTML</span>
-          <span>CSS</span>
-          <span>Bootstrap</span>
-          <span>JQuery</span>
-          <span>Handlebars.js</span>
-          <span>JSON</span>
-          <span>AJAX</span>
-          <span>APIs</span>
-          <span>The DOM</span>
-        </div>
-      </div>
+      <div className="resume-div">
+        <div className="skill-div">
+          {/* --------Frontend Skills------------ */}
+          <div className="skill">
+            <h2>Frontend Skills</h2>
+            <div className="tag">
+              <span>JavaScript</span>
+              <span>React</span>
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>Bootstrap</span>
+              <span>JQuery</span>
+              <span>Handlebars.js</span>
+              <span>JSON</span>
+              <span>AJAX</span>
+              <span>APIs</span>
+              <span>The DOM</span>
+            </div>
+          </div>
 
-      {/* --------Backtend Skills------------ */}
-      <div>
-        <h2>Backtend Skills</h2>
-        <div>
-          <span>Node.js</span>
-          <span>Express.js</span>
-          <span>GraphQL</span>
-          <span>MySQL</span>
-          <span>Sequelize</span>
-          <span>NoSQL</span>
-          <span>MongoDB</span>
-          <span>Mongoose</span>
-          <span>Testing</span>
-          <span>Heroku</span>
+          {/* --------Backtend Skills------------ */}
+          <div className="skill">
+            <h2>Backtend Skills</h2>
+            <div className="tag">
+              <span>Node.js</span>
+              <span>Express.js</span>
+              <span>GraphQL</span>
+              <span>MySQL</span>
+              <span>Sequelize</span>
+              <span>NoSQL</span>
+              <span>MongoDB</span>
+              <span>Mongoose</span>
+              <span>Testing</span>
+              <span>Heroku</span>
+            </div>
+          </div>
         </div>
+        {/* download CV */}
+        <a href={CV} download className="download">
+          Download CV
+          <img
+            alt="download"
+            style={{ display: "inline" }}
+            src={download}
+            className="download-icon"
+          />
+        </a>
       </div>
     </div>
   );
