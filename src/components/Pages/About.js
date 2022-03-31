ceee5c968f2a7a7fdc0edc722499d8df6ff97f51
Wenwen Tian <wwtian9@gmail.com>
2022-03-30 18:30:51 -0600

    add about me style

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -7,29 +7,31 @@ import myImage from "../../assets/images/me.JPG";
 export default function About() {
   return (
     <Container>
-      <h2>About Me</h2>
+      <h2 className="about">About Me</h2>
       <Row>
         <Col sm={4}>
           <img src={myImage} alt="profile" className="profile" />
         </Col>
         <Col sm={8}>
-          <p>
-            I am Wenwen, a full stack developer. <br />I am currently based in
-            Salt Lake City, Utah.
-          </p>
-          <p>
-            I have extensive knowledge of JavaScript, React.js, Node.js, and
-            MongoDB.
-            <br />I am passionate about web development, and will continue to
-            explore new technologies in the industry.
-          </p>
-          <p>I enjoy both outdoor and indoor activities:</p>
-          <ul>
-            <li>Travelling</li>
-            <li>Camping</li>
-            <li>Playing Games</li>
-            <li>Playing with My Dog</li>
-          </ul>
+          <div className="description">
+            <p>
+              I am Wenwen, a full stack developer. <br />I am currently based in
+              Salt Lake City, Utah.
+            </p>
+            <p>
+              I have extensive knowledge of JavaScript, React.js, Node.js, and
+              MongoDB.
+              <br />I am passionate about web development, and will continue to
+              explore new technologies in the industry.
+            </p>
+            <p>I enjoy both outdoor and indoor activities:</p>
+            <ul>
+              <li>Travelling</li>
+              <li>Camping</li>
+              <li>Playing Games</li>
+              <li>Playing with My Dog</li>
+            </ul>
+          </div>
         </Col>
       </Row>
     </Container>
