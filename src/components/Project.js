2168bc5c8d2c3e605dc95806d04a6dd7413d3428
Wenwen Tian <wwtian9@gmail.com>
2022-04-01 15:40:09 -0600

    add media query to project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,18 +3,18 @@ import githubIcon from "./../assets/icons/gh-black.png";
 
 export default function Project(props) {
   return (
-    <div className="card px-0 project-card col-xl-3 col-lg-4 col-md-5 col-sm-7 col-8">
+    <div className="card px-0 project-card col-xl-3 col-lg-4 col-md-4 col-sm-6 col-7">
       <div className="project-img ">
         <img alt="project" className="card-img-top" src={props.image} />
       </div>
       <div className="card-body row">
-        <div className="col-sm-9">
+        <div className="col-sm-9 col-9">
           <a className="project-title" href={props.deploy}>
             {props.title}
           </a>
           <p className="project-tech">{props.tech}</p>
         </div>
-        <div className="col-sm-3 github-div">
+        <div className="col-sm-3 col-3 github-div">
           <a href={props.github}>
             <img className="github" src={githubIcon} alt="github icon"/>
           </a>
