72ab4534bdd1407cb0d5165c274c76e73c4a2d7d
Wenwen Tian <wwtian9@gmail.com>
2022-04-01 22:04:47 -0600

    Updates

---
+++ precache-manifest.71668642bbaaba296d2487d543464e17.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "792e7abf2d68537075fd",
+    "url": "/portfolio-React/static/css/main.ee2065ee.chunk.css"
+  },
+  {
+    "revision": "792e7abf2d68537075fd",
+    "url": "/portfolio-React/static/js/main.792e7abf.chunk.js"
+  },
+  {
+    "revision": "2d281c672a566deb1a05",
+    "url": "/portfolio-React/static/js/1.2d281c67.chunk.js"
+  },
+  {
+    "revision": "97a9b280d012c75234fd",
+    "url": "/portfolio-React/static/css/2.a7dedf87.chunk.css"
+  },
+  {
+    "revision": "97a9b280d012c75234fd",
+    "url": "/portfolio-React/static/js/2.97a9b280.chunk.js"
+  },
+  {
+    "revision": "1ab6bc733fda3b9d238b",
+    "url": "/portfolio-React/static/js/3.1ab6bc73.chunk.js"
+  },
+  {
+    "revision": "f8da08572f0519034b59",
+    "url": "/portfolio-React/static/js/runtime~main.f8da0857.js"
+  },
+  {
+    "revision": "eaa1321bfe391e02cd964fc64fd0639f",
+    "url": "/portfolio-React/static/media/web.eaa1321b.png"
+  },
+  {
+    "revision": "a6c7550f208b5d7694c6720d851384be",
+    "url": "/portfolio-React/static/media/me.a6c7550f.JPG"
+  },
+  {
+    "revision": "5d7b959867343fa4ecf2258e28cd56c1",
+    "url": "/portfolio-React/static/media/matrix.5d7b9598.jpg"
+  },
+  {
+    "revision": "3c9beddb35f4331de7dd82613dbea44e",
+    "url": "/portfolio-React/static/media/chrip.3c9beddb.webp"
+  },
+  {
+    "revision": "a1914ed274c2f190321a1f3644f2884d",
+    "url": "/portfolio-React/static/media/weather.a1914ed2.webp"
+  },
+  {
+    "revision": "dc4a56ec0f325f3e2fbc295081b04e23",
+    "url": "/portfolio-React/static/media/quiz.dc4a56ec.webp"
+  },
+  {
+    "revision": "e9914fcd72a004d91c83c76bbfcf08d6",
+    "url": "/portfolio-React/static/media/task.e9914fcd.jpeg"
+  },
+  {
+    "revision": "8a046ec3cb9be251a200ced5a380aea2",
+    "url": "/portfolio-React/static/media/run.8a046ec3.jpeg"
+  },
+  {
+    "revision": "ee30115726562bae9e60c81fa1e57a63",
+    "url": "/portfolio-React/static/media/send.ee301157.png"
+  },
+  {
+    "revision": "9afa81e9230cef46eafc10c4e46c7fbb",
+    "url": "/portfolio-React/static/media/CV_wwt.9afa81e9.pdf"
+  },
+  {
+    "revision": "0617ffa7a39c1f6608012cf2a7ef2996",
+    "url": "/portfolio-React/index.html"
+  }
+];
